33962fbe986e4c33c3b5c5d5df10dc2ff7679112
Hossein Yousefi <u.hossein@yahoo.com>
2018-11-05 23:23:33 +0100

    HW1: Further tests + fixes

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,11 +1,11 @@
 public class Task1 {
         
-    private static int give_me_one() {
+    private static int giveMeOne() {
         return 1;
     }
     
-    public static void Main(String[] args) {
-        int one = give_me_one();
+    public static void main(String[] args) {
+        int one = giveMeOne();
         int two = one * 2;
         System.out.println(two);
     }
