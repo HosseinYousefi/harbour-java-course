581901eeb95363942444adeb4e3046d05734881a
Hossein Yousefi <u.hossein@yahoo.com>
2018-11-05 23:15:19 +0100

    HW1

---
+++ src/main/java/Homework1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class Homework1 {
+    
+    private static int[] readFile() {
+        try {
+            Scanner sc = new Scanner(new File("input.txt"));
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            return new int[] { a, b };
+        } catch (FileNotFoundException ex) {
+            return null;
+        }
+    }
+    
+    private static void writeFile(long number) {
+        try {
+            Writer writer = new BufferedWriter(
+                    new OutputStreamWriter(
+                        new FileOutputStream("output.txt"), "ascii"));
+            writer.write(Long.valueOf(number).toString());
+            writer.close();
+          } catch (IOException ex) {
+              
+          }
+    }
+
+    
+    public static void main(String[] args) {
+        int[] vals = readFile();
+        long mult = (long)vals[0] * vals[1];
+        writeFile(mult);
+    }
+}
